87c4dd5818cd98d2cc70315bbb5d20c50362dcea
MadisonViaLux <madison.s.mckown@gmail.com>
2021-08-21 20:06:30 -0700

    setting up basic api to return basic json

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // implement your API here
+
+const express = require('express');
+
+const server = express();
+
+// server.use(express.json())
+
+
+server.get('/', (req, res) => {
+    // let myJson = req.body;
+    res.send('Hello World');
+});
+
+
+server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+            id: 1,
+            name: 'Samwise'
+        },
+        {
+            id: 2,
+            name: 'Frodo'
+        }
+    ];
+
+    res.status(200).json(hobbits);
+
+})
+
+
+server.listen(5000, () => {
+    console.log(`server running on port 5000`);
+});
