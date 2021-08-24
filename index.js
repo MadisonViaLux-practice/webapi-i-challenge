96ef1494d3e0d3c8344a01d7edd897179e869808
MadisonViaLux <madison.s.mckown@gmail.com>
2021-08-23 19:29:22 -0700

    Creating simple CRUD tot test Postman and Insomnia

--- index.js
+++ index.js
@@ -4,30 +4,28 @@ const express = require('express');
 
 const server = express();
 
-// server.use(express.json())
+server.use(express.json())
 
 
 server.get('/', (req, res) => {
-    // let myJson = req.body;
     res.send('Hello World');
 });
 
-
 server.get('/hobbits', (req, res) => {
-    const hobbits = [
-        {
-            id: 1,
-            name: 'Samwise'
-        },
-        {
-            id: 2,
-            name: 'Frodo'
-        }
-    ];
+    res.send('Welcome')
+});
 
-    res.status(200).json(hobbits);
+server.post('/hobbits', (req, res) => {
+    res.status(201).json({url: '/hobbits', operation: 'POST'});
+});
 
-})
+server.put('/hobbits', (req, res) => {
+    res.status(200).json({url: '/hobbits', operation: 'PUT'});
+});
+
+server.delete('/hobbits', (req, res) => {
+    res.sendStatus(204);
+});
 
 
 server.listen(5000, () => {
